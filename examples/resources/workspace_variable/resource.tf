1574d1e23a5b6fea1d870a966283ff96c38d9b39
Alfredo España <alfespa17@gmail.com>
2024-05-01 15:55:16 -0600

    Add support to handle workspace variables

--- examples/resources/workspace_variable/resource.tf
+++ examples/resources/workspace_variable/resource.tf
@@ -1,10 +1,21 @@
 resource "terrakube_workspace_variable" "sample1" {
   organization_id = data.terrakube_organization.org.id
-  workspace_id    = data.terrakube_workspace.workspace.id
+  workspace_id    = terrakube_workspace_cli.sample1.id
   key             = "sample-env-var"
-  value           = "sample-value"
+  value           = "sample-value2222"
   description     = "sample env var"
   category        = "ENV"
+  sensitive       = false
+  hcl             = false
+}
+
+resource "terrakube_workspace_variable" "sample2" {
+  organization_id = data.terrakube_organization.org.id
+  workspace_id    = terrakube_workspace_cli.sample1.id
+  key             = "sample-terra-var"
+  value           = "sample-TERRAFORM"
+  description     = "sample env var"
+  category        = "TERRAFORM"
   sensitive       = false
   hcl             = false
 }
