abc8d19496e7de11884c69f2e3d0b13c68afd534
Benjamin Decreusefond <benjamin.decreusefond@weezevent.com>
2025-02-10 13:38:29 +0100

    fix: log message

--- internal/provider/module_resource.go
+++ internal/provider/module_resource.go
@@ -159,7 +159,7 @@ func (r *ModuleResource) Create(ctx context.Context, req resource.CreateRequest,
 	}
 
 	if !plan.TagPrefix.IsNull() {
-		tflog.Info(ctx, fmt.Sprintf("Module using Tag Prefix: %s", plan.VcsId.ValueString()))
+		tflog.Info(ctx, fmt.Sprintf("Module using Tag Prefix: %s", plan.TagPrefix.ValueStringPointer()))
 		bodyRequest.TagPrefix = plan.TagPrefix.ValueStringPointer()
 	}
 
@@ -327,7 +327,7 @@ func (r *ModuleResource) Update(ctx context.Context, req resource.UpdateRequest,
 	}
 
 	if !plan.TagPrefix.IsNull() {
-		tflog.Info(ctx, fmt.Sprintf("Module using Vcs connection id: %s", plan.VcsId.ValueString()))
+		tflog.Info(ctx, fmt.Sprintf("Module using Tag Prefix: %s", plan.TagPrefix.ValueStringPointer()))
 		bodyRequest.TagPrefix = plan.TagPrefix.ValueStringPointer()
 	}
 
